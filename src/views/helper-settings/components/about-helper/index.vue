e54ca6853467f72b8e120ef1e7c3769fc25aae6f
ayang <473033518@qq.com>
2022-08-01 18:10:24 +0800

    feat(helper-settings): complete the basic layout of the page

---
+++ src/views/helper-settings/components/about-helper/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about-helper">
+    about-helper
+  </div>
+</template>
--- src/views/helper-settings/index.vue
+++ src/views/helper-settings/index.vue
@@ -1,30 +1,102 @@
 <script setup lang="ts">
-import { openNewWindow } from "@/utils/tauriApi";
+import AboutHelper from "./components/about-helper/index.vue";
+import type { Component } from "vue";
+import type { MenuOption } from "naive-ui";
+
+// 当前选中菜单的对应组件
+const currentComponent = shallowRef<Component>();
+
+/**
+ * 给菜单列表每一项设置 key
+ * @param options 菜单项
+ */
+const setMenuOptionsKey = (options: MenuOption[]) =>
+  options.map((item, index) => ({
+    key: `${index}`,
+    ...item,
+    children: item?.children?.map((childItem, childIndex) => ({
+      key: `${index}-${childIndex}`,
+      ...childItem
+    }))
+  }));
+
+// 菜单项
+const menuOptions = setMenuOptionsKey([
+  {
+    label: "助手首页"
+  },
+  {
+    label: "账户设置",
+    children: [
+      {
+        label: "主播信息"
+      },
+      {
+        label: "管理信息"
+      }
+    ]
+  },
+  {
+    label: "弹幕助手",
+    children: [
+      {
+        label: "弹幕设置"
+      },
+      {
+        label: "自动回复"
+      },
+      {
+        label: "语音播报"
+      }
+    ]
+  },
+  {
+    label: "点歌助手"
+  },
+  {
+    label: "粉丝助手"
+  },
+  {
+    label: "时钟助手"
+  },
+  {
+    label: "提示助手"
+  },
+  {
+    label: "关于助手",
+    component: AboutHelper
+  }
+]);
+
+// 菜单的 key 发生变化
+const changeMenuKey = (key: string, item: any) => {
+  currentComponent.value = item.component;
+};
 </script>
 
 <template>
-  <div class="helper-settings">
-    helper-settings
-
-    <n-button @click="openNewWindow('/barrage')">
-      打开弹幕姬
-    </n-button>
-    <n-button @click="openNewWindow('/fans')">
-      打开粉丝姬
-    </n-button>
-    <n-button @click="openNewWindow('/music')">
-      打开音乐姬
-    </n-button>
-    <n-button @click="openNewWindow('/clock')">
-      打开时钟姬
-    </n-button>
-    <n-button @click="openNewWindow('/prompt')">
-      打开提示姬
-    </n-button>
-    <n-button @click="openNewWindow('/websocket')">
-      开启直播连接
-    </n-button>
+  <div class="helper-settings h-screen">
+    <n-layout
+      has-sider
+      class="h-full"
+    >
+      <n-layout-sider
+        bordered
+        :native-scrollbar="false"
+        :width="185"
+      >
+        <n-menu
+          default-value="0"
+          :options="menuOptions"
+          @update-value="changeMenuKey"
+        />
+      </n-layout-sider>
+      <n-layout-content>
+        <component
+          :is="currentComponent"
+          class="p-1.5"
+        />
+      </n-layout-content>
+    </n-layout>
   </div>
 </template>
-
-<style scoped lang="scss"></style>
