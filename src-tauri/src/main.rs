a2d49065cf705d0c33bd045628ce030fcb9b22a1
ayang <75017711+bilibili-ayang@users.noreply.github.com>
2022-08-03 22:06:40 +0800

    feat: add system tray (#9)
    Co-authored-by: Kaichen <276126767@qq.com>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,15 @@
   windows_subsystem = "windows"
 )]
 
+mod tray;
+
 fn main() {
+  let content = tauri::generate_context!();
+  
   use tauri::Manager;
   tauri::Builder::default()
+  .system_tray(tray::menu())  // ✅ 将 `tauri.conf.json` 上配置的图标添加到系统托盘
+  .on_system_tray_event(tray::handler) // ✅ 注册系统托盘事件处理程序
   .setup(|_app| {
     #[cfg(debug_assertions)] // only include this code on debug builds
     {
@@ -15,6 +21,6 @@ fn main() {
     }
     Ok(())
   })
-  .run(tauri::generate_context!())
+  .run(content)
   .expect("error while running tauri application");
 }
